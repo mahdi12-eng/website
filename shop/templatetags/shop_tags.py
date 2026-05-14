42ca9682c53295bd5d1dcc3a8abd68fb88e27ca1
engineerhashimi9 <engineerhashimi9@gmail.com>
2026-05-14 16:36:23 +0430

    Refactor forms and templates: improve field formatting, add date of birth input, and implement mobile and desktop navigation bars

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -1,8 +1,38 @@
 from django import template
 import datetime as dt
+from ..forms import LoginForm, RegisterForm
+from ..models import Categories
+
 register = template.Library()
+
 
 @register.simple_tag()
 def cur_date():
-    current_date=dt.date.today()
-    return 'It Works!'
+    current_date = dt.date.today()
+    return "It Works!"
+
+
+@register.inclusion_tag("product_info.html")
+def product_detail(post):
+    return post
+
+
+@register.inclusion_tag("form.html")
+def form(mode):
+    if mode == "login":
+        pass
+    else:
+        pass
+    return form
+
+
+@register.inclusion_tag("shop/navigations/side_bar.html")
+def load_category_list():
+    cat = Categories.objects.all()
+    return {"categories": cat}
+
+
+@register.inclusion_tag("shop/navigations/mobile.html")
+def load_category_list():
+    cat = Categories.objects.all()
+    return {"categories": cat}
